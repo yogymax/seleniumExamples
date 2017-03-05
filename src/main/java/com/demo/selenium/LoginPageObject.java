66ad05d416771d6182c921a0780c18db64a3daa6
Demo <demo>
2017-03-05 14:08:30 +0530

    Automation Changes

--- src/main/java/com/demo/selenium/LoginPageObject.java
+++ src/main/java/com/demo/selenium/LoginPageObject.java
@@ -1,6 +1,76 @@
 package com.demo.selenium;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.testng.Assert;
+
 public class LoginPageObject {
 
-	//http://filehippo.com/download_firefox/history/
+	private static final String REGISTRATIONTEXT ="Registration Form For Sports";
+	
+	@FindBy(id="abcd")
+	WebElement registrationLabel;
+	
+	@FindBy(id="firstname")
+	WebElement firstName;
+	
+	@FindBy(id="lastname")
+	WebElement lastName;
+	
+	@FindBy(id="dob")
+	WebElement dob;
+	
+	@FindBy(id="address")
+	WebElement address;
+	
+	@FindBy(id="phone")
+	WebElement phone;
+	
+	@FindBy(id="email")
+	WebElement email;
+	
+	@FindBy(id="zipcode")
+	WebElement zip;
+	
+	WebElement firstNameInput;
+	WebElement lastNameInput;
+	WebElement dobInput;
+	WebElement addressInput;
+	WebElement phoneInput;
+	WebElement emailInput;
+	WebElement zipcodeInput;
+	
+	@FindBy(xpath="/html/body/table[3]/tbody/tr/td/input")
+	WebElement registerBtn;
+	
+	public void validateRegisterPage(){
+		Assert.assertEquals(registrationLabel.getText(), REGISTRATIONTEXT);
+		Assert.assertEquals(registerBtn.getText(), "register");
+	}
+
+	public void fillRegistrationDetails
+	(String fname,String lname,String db,String addr, long phoneN)
+	{
+		firstName.clear();
+		firstName.sendKeys(fname);
+		
+		lastName.clear();
+		lastName.sendKeys(lname);
+		
+		dob.clear();
+		dob.sendKeys(db);
+		
+		address.clear();
+		address.sendKeys(addr);
+		
+		phone.clear();
+		phone.sendKeys(String.valueOf(phoneN));
+		
+		email.clear();
+		email.sendKeys(addr);
+		
+		zip.clear();
+		zip.sendKeys(fname);
+	}
+	
 }
---
+++ src/test/java/com/demo/selenium/RegisterationTest.java
@@ -0,0 +1,35 @@
+package com.demo.selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class RegisterationTest {
+	
+	private static final String appUrl = "C:\\Users\\Progvaltion_11\\git\\seleniumExamples\\src\\main\\java\\com\\others\\sample.html";
+	LoginPageObject loginInPO =null;
+	@BeforeClass
+	public void myMethod() {
+	  WebDriver driver = new FirefoxDriver();
+	  driver.get(appUrl);
+	  loginInPO = PageFactory.initElements
+	  (driver, LoginPageObject.class);
+	}
+	
+	
+	
+  @Test
+  public void registrationPageTest() {
+	  loginInPO.fillRegistrationDetails("A", "B", 
+			  "13213", "31231", 1232141);
+  }
+  
+  @Test
+  public void validateRegPage() {
+	  loginInPO.validateRegisterPage();
+  }
+  
+
+}
