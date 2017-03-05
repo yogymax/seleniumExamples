23a50552d8f5d5d19c2ee934e7b5d240206e9dbc
Demo <demo>
2017-03-05 12:16:17 +0530

    my message

---
+++ src/main/java/com/demo/selenium/LoginPageObject.java
@@ -0,0 +1,5 @@
+package com.demo.selenium;
+
+public class LoginPageObject {
+
+}
---
+++ src/test/java/com/demo/selenium/TestLoginModule.java
@@ -0,0 +1,15 @@
+package com.demo.selenium;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+
+public class TestLoginModule {
+	
+  @Test
+  public void f() {
+  }
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+}
